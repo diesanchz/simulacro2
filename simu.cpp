1d0e9d6a3f6b46526e49abd4d2ecfecdc089534e
diesanchz <00090725@uca.edu.sv>
2025-06-05 09:52:08 -0600

    00090725

--- simu.cpp
+++ simu.cpp
@@ -1,1 +1,18 @@
-#include 
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"ingrse";
+    cin>>n;
+   for(int i=1; i<= n; i++){
+    
+     for (int j=1;j<=i;j++){
+      cout<<"*";
+
+     }
+   }
+
+
+
+    return 0;
+}
